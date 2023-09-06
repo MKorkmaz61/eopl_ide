a1207c8b231baf98b9c81db0b873b93e70dfc540
Muhammed Korkmaz <mkorkmaz95@gmail.com>
2023-09-07 01:52:20 +0300

    Screenshots added

--- Language/call-by-reference/result.scm
+++ Language/call-by-reference/result.scm
@@ -1,10 +1,18 @@
 #lang racket
 (require "top.scm")
 (require "globalstack.scm")
-(run "let p = proc (x) set x = 4
-      in let a = 3
-         in begin (p a); a end
-")
+(run "let swap = proc (x) proc (y)
+                      let temp = x
+                      in begin 
+                          set x = y;
+                          set y = temp
+                         end
+         in let a = 33
+         in let b = 44
+         in begin
+             ((swap a) b);
+             -(a,b)
+            end")
 (define print-element (lambda (i) (pretty-print i) (display "\n*element*\n")))
 (display "\n**split**\n")
 (map print-element (get-expstack))
